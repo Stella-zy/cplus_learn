e10f7c71fef1f5a0300c2c15fd5c94315f66cb4d
zxy30 <108453609@qq.com>
2017-10-19 18:59:32 +0800

    c plus

---
+++ initLeanCloud.js
@@ -0,0 +1,8 @@
+// LeanCloud - 初始化 - 将这里的 APP_ID 和 APP_KEY 替换成自己的应用数据
+// https://leancloud.cn/docs/sdk_setup-js.html#初始化
+var APP_ID = 'niFB3gXNf8u6cxilYnnCs9Sh-gzGzoHsz';
+var APP_KEY = 'iuCNCXxWmGi874quUkRDdM1h';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
